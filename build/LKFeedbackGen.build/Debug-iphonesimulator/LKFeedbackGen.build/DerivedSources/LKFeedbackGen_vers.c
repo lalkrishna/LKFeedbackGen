ff3685850e7dc685359db55a4087dd88a32906a2
Lal Krishna <lalkrishna@accubits.com>
2020-07-09 14:14:22 +0530

    Ignored files removed

--- build/LKFeedbackGen.build/Debug-iphonesimulator/LKFeedbackGen.build/DerivedSources/LKFeedbackGen_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char LKFeedbackGenVersionString[];
- extern const double LKFeedbackGenVersionNumber;
-
- const unsigned char LKFeedbackGenVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:LKFeedbackGen  PROJECT:LKFeedbackGen-1" "\n";
- const double LKFeedbackGenVersionNumber __attribute__ ((used)) = (double)1.;
--- build/LKFeedbackGen.build/Debug-iphonesimulator/LKFeedbackGen.build/Objects-normal/i386/LKFeedbackGen_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/developer/Documents/PROJECTS/LKFeedbackGen/build/LKFeedbackGen.build/Debug-iphonesimulator/LKFeedbackGen.build/DerivedSources/LKFeedbackGen_vers.c
